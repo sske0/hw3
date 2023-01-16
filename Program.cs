433c480112995d4e324857b179487ef6cc2caffc
sske0 <akbaraz@code.edu.az>
2023-01-16 06:00:43 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //Console.Write("array size: ");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int[] numbers = new int[n];
+        //int sum = 0;
+
+        //for (int i = 0; i < n; i++)
+        //{
+        //    Console.WriteLine($"enter element {i}: ");
+        //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+        //    sum += numbers[i];
+
+        //}
+        //Console.WriteLine(sum / n);
+
+
+
+
+
+        //Console.Write("array size: ");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int[] numbers = new int[n];
+        //int temp = 0;
+        //int j = 0;
+        //int i = 0;
+
+        //for (i = 0; i < n; i++)
+        //{
+        //    Console.WriteLine($"enter element {i}: ");
+        //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+        //}
+
+
+
+
+        //j = i - 1;
+        //for (i = 0; i < j; i++)
+        //{
+
+        //    temp = numbers[i];
+        //    numbers[i] = numbers[j];
+        //    numbers[j] = temp;
+        //    j--;
+        //}
+        //for (i = 0; i < n; i++)
+        //{
+        //    Console.WriteLine(numbers[i]);
+        //}
+
+
+
+//        Console.Write("array size: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+//        int[] numbers = new int[n];
+//        int temp = 0;
+//        int j = 0;
+//        int i = 0;
+
+//        for (i = 0; i < n; i++)
+//        {
+//            Console.WriteLine($"enter element {i}: ");
+//            numbers[i] = Convert.ToInt32(Console.ReadLine());
+          
+//        }
+//        for (i = 0; i < n; i++)
+//        {
+//            for (j = i + 1; j < n; j++)
+//            {
+//                if (numbers[i] > numbers[j])
+//                {
+//                    temp = numbers[j];
+//                    numbers[j] = numbers[i];
+//                    numbers[i] = temp;
+//                }
+//            }
+//        }
+
+//        for (i =0; i < n; i++)
+//        {
+//            Console.WriteLine(numbers[i]);
+//        }
+
+
+
+
+
+//        }
+//}
+
+
+
+
+
+
